a5aae9b613ea3508b0b8984b35e348b3b49c0e4c
Uzair Ahmad <42337244+uzair657@users.noreply.github.com>
2022-02-12 18:00:59 +0500

    Colors Updated

--- lib/main.dart
+++ lib/main.dart
@@ -3,93 +3,97 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/painting.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: 'Uzair Ahmad',
-    home: Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          'Personal Information',
-          style: TextStyle(color: Colors.white),
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Uzair Ahmad',
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            'Personal Information',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.teal,
         ),
-        backgroundColor: Colors.teal,
-      ),
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            //Avatar Data
-            CircleAvatar(
-              radius: 50.0,
-              backgroundImage: AssetImage('images/avatar.jpg'),
-            ),
-            //Title NAME
-            Text(
-              'Uzair Ahmad',
-              style: TextStyle(
-                  fontSize: 40,
-                  fontFamily: 'Pacifico',
-                  fontWeight: FontWeight.bold,
-                  color: Colors.teal),
-            ),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //Avatar Data
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/avatar.jpg'),
+              ),
+              //Title NAME
+              Text(
+                'Uzair Ahmad',
+                style: TextStyle(
+                    fontSize: 40,
+                    fontFamily: 'Pacifico',
+                    fontWeight: FontWeight.w600,
+                    color: Colors.teal.shade300),
+              ),
 
-            //Post or Your Job
-            Text(
-              'FLUTTER DEVELOPER',
-              style: TextStyle(
-                fontFamily: 'Source Sans Pro',
-                fontSize: 15.0,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2.5,
-                color: Colors.teal.shade50,
+              //Post or Your Job
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                  color: Colors.teal.shade50,
+                ),
               ),
-            ),
-            SizedBox(
-              height: 20.0,
-              width: 120.0,
-              child: Divider(
-                color: Colors.teal.shade100,
+              SizedBox(
+                height: 20.0,
+                width: 120.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
               ),
-            ),
-            //Card Phone
-            Card(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    color: Colors.teal,
-                  ),
-                  title: Text(
-                    '+92 309 72500 69',
-                    style: TextStyle(
+              //Card Phone
+              Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
                       color: Colors.teal,
-                      fontSize: 20.0,
-                      fontFamily: 'Source Sans Pro',
                     ),
-                  ),
-                )),
-            //Card Email
-            Card(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.email,
-                    color: Colors.teal,
-                  ),
-                  title: Text(
-                    'ahmaduzair355@gmail.com',
-                    style: TextStyle(
+                    title: Text(
+                      '+92 309 72500 69',
+                      style: TextStyle(
+                        color: Colors.teal,
+                        fontSize: 20.0,
+                        fontFamily: 'Source Sans Pro',
+                      ),
+                    ),
+                  )),
+              //Card Email
+              Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
                       color: Colors.teal,
-                      fontSize: 20.0,
-                      fontFamily: 'Source Sans Pro',
                     ),
-                  ),
-                )),
-          ],
+                    title: Text(
+                      'ahmaduzair355@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal,
+                        fontSize: 20.0,
+                        fontFamily: 'Source Sans Pro',
+                      ),
+                    ),
+                  )),
+            ],
+          ),
         ),
       ),
     ),
-  ));
+  );
 }
